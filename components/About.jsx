5155952e1c64057e53aaa9e4e906cceccd9aea7c
mbakoG90210 <mbakogoitsemang@live.com>
2024-10-09 19:37:54 +0200

    3. About page: Updated About us section. To Add: Packages and order procedure

--- components/About.jsx
+++ components/About.jsx
@@ -46,7 +46,8 @@ const About = () => {
               decker camper bus tailor made to meet the needs of various
               clients. The bus can carry up to 18 passengers with dining room
               designed for 8.
-              <h4>*** What's in the bus Lower deck ***</h4> Dining room with 32
+              <h4>*** What's in the bus Lower deck ***</h4>
+               Dining room with 32
               Inch TV, sofas, aircon mini whiskey bar Sleeping room with 4
               movable bunk beds each with 32inch TV, air con. Kitchen with
               sink,fridge,microwave,8 bottle wine cooler, ice maker, 4 plate
@@ -56,11 +57,31 @@ const About = () => {
               sharing can sleep in each Whiskey bar Verandar with seating space
               for 10. Verandar can be extended up to 3 metres high when bus
               stationery.
+              <h3>Available Packages</h3>
+              <ul>
+                <li>1. Hotel On Wheels Bus Only </li>
+                <li>2. limousine hire only </li>
+                <li>3. Combo of HOW Bus and limousine </li>
+                <li>** executive 4x4 cars available on request</li>
+              </ul>
             </p>
             {/* stats */}
             <div className="my-5 xl:my-10 min-h-[35px]">
               {inView && <Stats />}
             </div>
+            <p>
+              <h3> How to hire our services</h3>
+              <ul>
+                <li>1. Choose your preferred hire dates</li>
+                <li>2. Discuss your planned route for roads' suitability </li>
+                <li>3. Book your dates based on availity </li>
+                <li>4. Bus comes with a professional chauffer and an onboard hostess </li>
+              </ul>
+
+              We can  link you to various independent  Tour agents we work with  
+              if you need their services for make your hire period even more memorable
+              with added activities and sightings. 
+            </p>
           </motion.div>
         </div>
       </div>
